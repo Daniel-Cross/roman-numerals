8fdb192673d35deb986c1bbe2134fbe7b801a798
Daniel Cross <crossy1686@gmail.com>
2019-05-05 21:34:29 +0200

    code for remaning numerals added along with testing

--- index.js
+++ index.js
@@ -1,17 +1,35 @@
-const Numerals = (number) => {
-	if (number === 1) {
-		return 'I';
-	} else if (number === 2) {
-		return 'II';
-	} else if (number === 3) {
-		return 'III';
-	} else if (number === 4) {
-		return 'IV';
-	} else if (number === 5) {
-		return 'V';
-	} else {
-		return 'Must be a number between 1 and 3999';
-	}
+const ROMAN = {
+	M: 1000,
+	CM: 900,
+	D: 500,
+	CD: 400,
+	C: 100,
+	XC: 90,
+	L: 50,
+	XL: 40,
+	X: 10,
+	IX: 9,
+	V: 5,
+	IV: 4,
+	I: 1,
 };
 
-module.exports = Numerals;
+const numerals = (number) => {
+	if (number < 1 || number > 3999 || isNaN(number)) {
+		throw new Error('Value must be a number between 1 and 3999');
+	}
+
+	let result = '';
+	const numerals = Object.keys(ROMAN);
+
+	numerals.forEach((key) => {
+		if (number >= ROMAN[key]) {
+			result += key.repeat(Math.floor(number / ROMAN[key]));
+			number -= ROMAN[key] * Math.floor(number / ROMAN[key]);
+		}
+	});
+
+	return result;
+};
+
+module.exports = numerals;
--- index.test.js
+++ index.test.js
@@ -1,31 +1,50 @@
-const Numerals = require('./index');
+const numerals = require('./index');
 
-describe('ROMAN_NUMERALS', () => {
-	it('returns the numeral I when 1 is entered', () => {
-		expect(Numerals(1)).toBe('I');
+describe('numerals', () => {
+	const testCases = [
+		[ 1, 'I' ],
+		[ 2, 'II' ],
+		[ 3, 'III' ],
+		[ 4, 'IV' ],
+		[ 5, 'V' ],
+		[ 6, 'VI' ],
+		[ 7, 'VII' ],
+		[ 8, 'VIII' ],
+		[ 9, 'IX' ],
+		[ 10, 'X' ],
+		[ 11, 'XI' ],
+		[ 12, 'XII' ],
+		[ 40, 'XL' ],
+		[ 46, 'XLVI' ],
+		[ 50, 'L' ],
+		[ 59, 'LIX' ],
+		[ 90, 'XC' ],
+		[ 93, 'XCIII' ],
+		[ 100, 'C' ],
+		[ 141, 'CXLI' ],
+		[ 165, 'CLXV' ],
+		[ 402, 'CDII' ],
+		[ 500, 'D' ],
+		[ 575, 'DLXXV' ],
+		[ 900, 'CM' ],
+		[ 999, 'CMXCIX' ],
+		[ 1000, 'M' ],
+		[ 1028, 'MXXVIII' ],
+		[ 3999, 'MMMCMXCIX' ],
+	];
+
+	testCases.forEach(([ input, expected ]) => {
+		it(`converts ${input} to ${expected}`, () => {
+			expect(numerals(input)).toBe(expected);
+		});
 	});
-
-	it('returns the numeral II when 2 is entered', () => {
-		expect(Numerals(2)).toBe('II');
+	it('throws an error on inputs greater than 3999', () => {
+		expect(() => numerals(4000)).toThrow('Value must be a number between 1 and 3999');
 	});
-
-	it('returns the numeral III when 3 is entered', () => {
-		expect(Numerals(3)).toBe('III');
+	it('throws an error on inputs less than 1', () => {
+		expect(() => numerals(0)).toThrow('Value must be a number between 1 and 3999');
 	});
-
-	it('returns the numeral IV when 4 is entered', () => {
-		expect(Numerals(4)).toBe('IV');
-	});
-
-	it('returns the numeral V when 5 is entered', () => {
-		expect(Numerals(5)).toBe('V');
-	});
-
-	it('returns an error message when a negative number is entered', () => {
-		expect(Numerals(-2)).toBe('Must be a number between 1 and 3999');
-	});
-
-	it('returns an error message when a number over 3999 entered', () => {
-		expect(Numerals(4000)).toBe('Must be a number between 1 and 3999');
+	it('throws an error if input is a string', () => {
+		expect(() => numerals('one')).toThrow('Value must be a number between 1 and 3999');
 	});
 });
